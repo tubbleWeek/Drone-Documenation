3514a535f16b092a5062c9ddfaad168362f60e43
William Chastek <166871893+tubbleWeek@users.noreply.github.com>
2024-05-16 10:27:22 -0500

    Add files via upload
    added c++ file for drone to move in square shape continuously

---
+++ GazeboSimulation/DroneSimulation/continuous_square.cpp
@@ -0,0 +1,77 @@
+#include <gnc_functions.hpp>
+//include API 
+
+int main(int argc, char** argv)
+{
+	//initialize ros 
+	ros::init(argc, argv, "gnc_node");
+	ros::NodeHandle gnc_node("~");
+	
+	//initialize control publisher/subscribers
+	init_publisher_subscriber(gnc_node);
+
+  	// wait for FCU connection
+	wait4connect();
+
+	//wait for used to switch to mode GUIDED
+	wait4start();
+
+	//create local reference frame 
+	initialize_local_frame();
+
+	//request takeoff
+	takeoff(2);
+
+	//specify some waypoints 
+	std::vector<gnc_api_waypoint> waypointList;
+	gnc_api_waypoint nextWayPoint;
+	nextWayPoint.x = 0;
+	nextWayPoint.y = 0;
+	nextWayPoint.z = 3;
+	nextWayPoint.psi = 0;
+	waypointList.push_back(nextWayPoint);
+	nextWayPoint.x = 5;
+	nextWayPoint.y = 0;
+	nextWayPoint.z = 3;
+	nextWayPoint.psi = -90;
+	waypointList.push_back(nextWayPoint);
+	nextWayPoint.x = 5;
+	nextWayPoint.y = 5;
+	nextWayPoint.z = 3;
+	nextWayPoint.psi = 0;
+	waypointList.push_back(nextWayPoint);
+	nextWayPoint.x = 0;
+	nextWayPoint.y = 5;
+	nextWayPoint.z = 3;
+	nextWayPoint.psi = 90;
+	waypointList.push_back(nextWayPoint);
+	nextWayPoint.x = 0;
+	nextWayPoint.y = 0;
+	nextWayPoint.z = 3;
+	nextWayPoint.psi = 180;
+	waypointList.push_back(nextWayPoint);
+	nextWayPoint.x = 0;
+	nextWayPoint.y = 0;
+	nextWayPoint.z = 3;
+	nextWayPoint.psi = 0;
+	waypointList.push_back(nextWayPoint);
+
+
+	//specify control loop rate. We recommend a low frequency to not over load the FCU with messages. Too many messages will cause the drone to be sluggish
+	ros::Rate rate(2.0);
+	int counter = 0;
+	while(ros::ok())
+	{
+		ros::spinOnce();
+		rate.sleep();
+		if(check_waypoint_reached(.3) == 1)// If drone has reached waypoint then go to the next waypoint specified
+		{
+			int x = counter % 4;// index for the point that we want the drone to go to
+			set_destination(waypointList[x].x,waypointList[x].y,waypointList[x].z, waypointList[x].psi);//waypointList is an array of 4-tuples (x, y, z, psi)
+			counter++;	
+	
+		}	
+		
+	}
+	return 0;
+}
