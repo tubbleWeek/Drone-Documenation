ad993a18e52543d687e5654ecd8ab0aaa0dea616
William Chastek <166871893+tubbleWeek@users.noreply.github.com>
2024-05-24 10:10:33 -0500

    Add files via upload

--- Phasespace/testrosbag.py
+++ Phasespace/testrosbag.py
@@ -3,16 +3,26 @@ import matplotlib
 import matplotlib.pyplot as plt
 import rosbag
 import rospy
+import sys
 
+if(len(sys.argv) != 2):
+    print("Usage: testrosbag.py $\{FILENAME\}")
+    sys.exit(-1)
+
+filename = sys.argv[1]
+
+#initialize the subplots
 fig, axs = plt.subplots(2, 4)
 
+#Lists to hold each LEDs positions
 xlist = []
 ylist = []
 zlist = []
 time = []
-bag = rosbag.Bag('posesv2.bag')
+bag = rosbag.Bag(filename)#Open the rosbag
 
 for i in range(0,8):
+    #intermediate lists to hold each LEDs position
     temp_xlist = []
     temp_ylist = []
     temp_zlist = []
@@ -22,18 +32,21 @@ for i in range(0,8):
         temp_xlist.append(msg.markers[i].x)
         temp_ylist.append(msg.markers[i].y)
         temp_zlist.append(msg.markers[i].z)
+    #Add the temp values to the list of positions
     xlist.append(temp_xlist)
     ylist.append(temp_ylist)
     zlist.append(temp_zlist)    
     time.append(temp_time)
 
-
+#done with Rosbag
 bag.close()
+#Convert all postions to a compatible format
 for i in range (0,8):
     xlist[i] = np.array(xlist[i])
     ylist[i] = np.array(ylist[i])
     zlist[i] = np.array(zlist[i])
     time[i] = np.array(time[i])
+#Add all the Data to the plots
 j = 0
 for i in range (0,8):
     if i == 4:
@@ -43,6 +56,7 @@ for i in range (0,8):
     axs[j,i%4].plot(time[i], zlist[i], label = "z position")
     axs[j,i%4].set_title(f'LED {i}')
 
+#Label the plots
 for ax in axs.flat:
     ax.set(xlabel='Time(seconds)', ylabel='Position(cm)')
 
@@ -51,6 +65,8 @@ for ax in axs.flat:
 
 plt.legend()
 
+#Display the final plot
 plt.show()
 
+#finished
 
